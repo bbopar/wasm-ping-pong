c79d8fb987ba4bd978263a99d1de2e39c5dd3f91
Benjamin Berta <benjamin.berta@barrage.net>
2021-09-06 09:14:13 +0200

    init commit

--- src/contract.rs
+++ src/contract.rs
@@ -1,72 +1,60 @@
+
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint64, to_binary};
 
 use crate::error::ContractError;
 use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, STATE};
+use crate::state::PING_COUNT;
+// use crate::state::{State, STATE};
+use std::ops::Add;
+use cosmwasm_std::StdError;
+use cosmwasm_std::attr;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
-    info: MessageInfo,
-    msg: InstantiateMsg,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    let state = State {
-        count: msg.count,
-        owner: info.sender.clone(),
-    };
-    STATE.save(deps.storage, &state)?;
-
-    Ok(Response::new()
-        .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+    PING_COUNT.save(deps.storage, &Uint64::zero())?;
+    let res = Response::new();
+    Ok(res)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     _env: Env,
-    info: MessageInfo,
+    _info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
+    let mut count = Uint64::zero();
     match msg {
-        ExecuteMsg::Increment {} => try_increment(deps),
-        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
-    }
-}
-
-pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
-
-    Ok(Response::new().add_attribute("method", "try_increment"))
-}
-pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        if info.sender != state.owner {
-            return Err(ContractError::Unauthorized {});
+        ExecuteMsg::Ping {} => {
+            let _increment: Result<Uint64, StdError> = PING_COUNT.update(deps.storage, |exists| {
+                count = exists.add(Uint64::from(1u8));
+                Ok(count)
+            });
+            let mut res = Response::new();
+            res.attributes.push(attr("ping_count", count));
+            // res.attributes.push(attr("pong", "pong"));
+            res.data = Some(to_binary("pong")?);
+            Ok(res)
         }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(Response::new().add_attribute("method", "reset"))
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-    match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
-    }
+pub fn query(deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    let count = PING_COUNT.load(deps.storage)?;
+    to_binary(&count)
 }
 
-fn query_count(deps: Deps) -> StdResult<CountResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(CountResponse { count: state.count })
+#[allow(dead_code)]
+fn query_count(_deps: Deps) -> StdResult<CountResponse> {
+    unimplemented!()
 }
 
 #[cfg(test)]
@@ -75,12 +63,13 @@ mod tests {
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
     use cosmwasm_std::{coins, from_binary};
 
+
     #[test]
     fn proper_initialization() {
         let mut deps = mock_dependencies(&[]);
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
+        let msg = InstantiateMsg {};
+        let info = mock_info("creator", &[]);
 
         // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
@@ -88,10 +77,35 @@ mod tests {
 
         // it worked, let's query the state
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+        let value: Uint64 = from_binary(&res).unwrap();
+        assert_eq!(Uint64::zero(), value);
     }
 
+    #[test]
+    fn test_ping() {
+        let mut deps = mock_dependencies(&[]);
+
+        let msg = InstantiateMsg {};
+        let info = mock_info("creator", &[]);
+
+        instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
+
+        let msg = ExecuteMsg::Ping {};
+
+        let res = execute(deps.as_mut(), mock_env(), info.clone(), msg.clone()).unwrap();
+        assert_eq!(res.attributes.len(), 1);
+        assert_eq!(res.attributes, vec![attr("ping_count", 1.to_string())]);
+        let data: String = from_binary(&res.data.unwrap()).unwrap();
+        assert_eq!(data, "pong");
+
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(res.attributes.len(), 1);
+        assert_eq!(res.attributes, vec![attr("ping_count", 2.to_string())]);
+        let data: String = from_binary(&res.data.unwrap()).unwrap();
+        assert_eq!(data, "pong");
+    }
+
+    /*
     #[test]
     fn increment() {
         let mut deps = mock_dependencies(&coins(2, "token"));
@@ -138,4 +152,5 @@ mod tests {
         let value: CountResponse = from_binary(&res).unwrap();
         assert_eq!(5, value.count);
     }
+    */
 }
